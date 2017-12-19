86aa544f319f751eddd29182a8a2ffe96fcb5823
Ivelise Sola <solaivelise@yahoo.com>
2017-12-19 15:22:53 -0500

    js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,14 @@
 function chooseWord (arr) {
     return arr[Math.floor(Math.random()*arr.length)];
 }
+
+function blanksFromAnswer (chooceWord) {
+    
+    var result = ""; // This is the variable we want to use
+    
+    // Write a loop here to concatanate a '_' to result for
+    // every letter in answerWord.
+    
+    
+    return result;
+}
