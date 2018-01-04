813a549fa05a95fc5260dac4a24e9f3b3f7e2276
Ivelise Sola <solaivelise@yahoo.com>
2018-01-03 23:45:55 -0500

    validations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
 
-    var arr = ["loop", "semicolon", "boolean", "array", "variable", "function", "method", "parameter"];
-    var validLetters =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "Z"];
+    var arr = ["c", "php", "java", "javascript", "pascal", "objetivec", "html" ,"python","swift", "csharp", "sql"];
+    var validLetters =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var word = chooseWord(arr);
     var wordSpaces = [];
     var spaces = blanksFromAnswer(word);
     var remainingLetters = word.length;
     var lettersAlreadyGuessed = [];
-    var newList = countElements(letter);
-    // var keyPressed = event.key.toLowerCase();
+    var failed = 0;
 
     drawWord();
 
@@ -20,8 +19,8 @@ $(document).ready(function () {
     }
 
 
-    function chooseWord(p1) {
-        return p1[Math.floor(Math.random() * p1.length)];
+    function chooseWord(parameter) {
+        return parameter[Math.floor(Math.random() * parameter.length)];
     }
 
 
@@ -46,12 +45,25 @@ $(document).ready(function () {
 
     $("#target").submit(function (event) {
         var letter = $("input:first").val();
-        if (letter.length !== 1) {
-            alert("Please enter a single letter.");
+        if (letter.length == 1 && validLetters.indexOf(letter) > -1) {
+            if(lettersAlreadyGuessed.indexOf(letter) < 0) {
+                if(word.indexOf(letter) > -1) {
+                    checkGuess(letter);
+                    list(letter);
+                }
+                else {
+                    // notInWord
+                    failed++;
+                    alert("Incorrect guess");
+                    list(letter);
+                    lostGame(failed);
+                }
+            }
+            else {
+                alert("You already guessed that letter");
+            }
         } else {
-            list(letter);
-            checkGuess(letter);
-            lostGame(newList);
+            alert("Please enter a single letter")
         }
         drawWord();
         event.preventDefault();
@@ -59,10 +71,10 @@ $(document).ready(function () {
     })
 
 
-    function checkGuess(p2) {
+    function checkGuess(parameter) {
         for (var i = 0; i < word.length; i++) {
-            if (word[i] === p2) {
-                wordSpaces[i] = p2;
+            if (word[i] === parameter) {
+                wordSpaces[i] = parameter;
                 remainingLetters--;
                 winner(remainingLetters);
             }
@@ -70,65 +82,32 @@ $(document).ready(function () {
     }
 
 
-    function list(p3) {
-        lettersAlreadyGuessed.push(p3);
+    function list(parameter) {
+        lettersAlreadyGuessed.push(parameter);
         var moreLetters = $(".guessedList");
         var newList = $("<div>");
+        var count = 11;
         for (var i = 0; i < lettersAlreadyGuessed.length; i++) {
             newList.text(lettersAlreadyGuessed[i]);
             moreLetters.append(newList);
+            count--;
         }
     }
 
-    function countElements(p6) {
-        // var failed = 0;
-        lettersAlreadyGuessed.push(p6);
-        
-        // for (var i = 0; i < lettersAlreadyGuessed.length; i++){
-        //     for ( var j = 0; j < word.length; j++)
-        //     if ( lettersAlreadyGuessed[i] != word[j]) {
-        //         failed++;
-        //     }
-        // }
-        return lettersAlreadyGuessed;
-        // return failed;
-    }
 
-    // var  keyPressed = event.key.toLowcase();
-    // if (letterGuessed.length === 5) {
-
-    // }
-    // if (validLetters.indexOf(keyPressed) > -1 ){
-   
-    //     if ( lettersAlreadyGuessed.indexOf(keyPressed > -1)){
-    //         alert("you already guessed that letter");
-    //     }else {
-    //         letterGuessed.push(keyPressed);
-    //     }
-    // }else {
-    //     alert("you did not pressed a valid key")
-    // }
-
-    function winner(p4) {
-        if (p4 === 0) {
+    function winner(parameter) {
+        if (parameter === 0) {
             $(".draw").remove();
             $(".guessedList").remove();
             $("#target").remove();
             var messageWinner = $(".winnerMessage");
             var newDiv = $(`<div>
                <img src ="https://www.arcamax.com/hangman/win.gif" alt="happy hangman">
-                <p>"Congrats, you won!"</p>
+                <p>"Congrats, you win!"</p>
                     <form id="target">
                         <input type="submit" value="Play Again!">
                     </form>
                 </div>`)
-                
-            // var newDiv = $("<div>");
-            // newDiv.text("Congrats you won!!!");
-            // messageWinner.append(newDiv);
-            // var newButton = $("<input type='button'/>");
-            // newButton.addClass("play");
-            // newButton.text("Play Again!");
             messageWinner.append(newDiv);
         }
     }
@@ -139,17 +118,12 @@ $(document).ready(function () {
     });
 
 
-
-    function lostGame(p) {
-        // lettersAlreadyGuessed.push(p);
-        if (p.length === 11) {
+    function lostGame(parameter) {
+        if (parameter === 11) {
             $(".draw").remove();
             $(".guessedList").remove();
             $("#target").remove();
             var messageLostGame = $(".lostGameMessage");
-            // var divLostGame = $("<div>");
-            // divLostGame.text("Oops! Sorry, You've lost!");
-            // messageLostGame.append(divLostGame);
             var newDiv = $(`<div>
             <img src = "https://thumbs.gfycat.com/SaneHeavyIrishwolfhound-max-1mb.gif" alt = "hangman">
             <p>"Oops, you've lost!"</p>
